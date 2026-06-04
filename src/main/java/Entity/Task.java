7ec4907e068e200de5c4265a7ff06010d0afc0e0
drack <artemsenuv@gmail.com>
2026-06-04 16:08:30 +0300

    create entity

---
+++ src/main/java/Entity/Task.java
@@ -0,0 +1,45 @@
+package Entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "task")
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Column(columnDefinition = "TEXT")
+    private String title;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    @Column(nullable = false)
+    private LocalDateTime dedline;
+
+@Column(name = "user_id", nullable = false)
+    private Long userId;
+
+@Column(name = "create_at", nullable = false,unique = false)
+    private LocalDateTime createAt;
+
+@PrePersist
+    protected void setTimeBeforeSave(){
+    this.createAt = LocalDateTime.now();
+
+}
+
+
+}
