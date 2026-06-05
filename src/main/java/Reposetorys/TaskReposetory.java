35b5bc6dc8b63821628320254540f610da938e92
drack <artemsenuv@gmail.com>
2026-06-05 15:15:19 +0300

    create logic TaskReposetory

---
+++ src/main/java/Reposetorys/TaskReposetory.java
@@ -0,0 +1,12 @@
+package Reposetorys;
+
+import Entity.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface TaskReposetory  extends JpaRepository<Task, Long> {
+List<Task> findByUserId(Long userId);
+List<Task> findByUserIdAndDeadlineAfter(Long userId, LocalDateTime dedlineAfter);
+}
