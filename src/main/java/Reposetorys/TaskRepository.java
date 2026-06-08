af1693a3c5274de222d5846c788a89d97130e8d6
drack <artemsenuv@gmail.com>
2026-06-08 14:53:02 +0300

    new method for prohibit create duplicate

--- src/main/java/Reposetorys/TaskRepository.java
+++ src/main/java/Reposetorys/TaskRepository.java
@@ -6,7 +6,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.time.LocalDateTime;
 import java.util.List;
 
-public interface TaskReposetory  extends JpaRepository<Task, Long> {
+public interface TaskRepository extends JpaRepository<Task, Long> {
 List<Task> findByUserId(Long userId);
 List<Task> findByUserIdAndDeadlineAfter(Long userId, LocalDateTime dedlineAfter);
+boolean existsByUserIdAndTitle(Long userId, String title);
 }
