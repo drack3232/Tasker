2cc2c3b87474d76e9236ad1416f05c10603e6a01
drack <artemsenuv@gmail.com>
2026-06-08 14:30:46 +0300

    create DTO

---
+++ src/main/java/DTO/TaskCreateDTO.java
@@ -0,0 +1,18 @@
+package DTO;
+
+import jakarta.validation.constraints.*;
+
+import java.time.LocalDateTime;
+
+public record TaskCreateDTO(
+        String description,
+
+        @NotBlank(message = "Here you can have title for task")
+        @Size(min = 3, max = 25)
+        String title,
+
+        @NotNull
+        @Future(message = "Deadline can`t be in past")
+        LocalDateTime deadline
+) {
+}
---
+++ src/main/java/DTO/TaskResponseDTO.java
@@ -0,0 +1,20 @@
+package DTO;
+
+import jakarta.validation.constraints.*;
+
+
+import java.time.LocalDateTime;
+
+public record TaskResponseDTO(
+        Long id,
+
+        @NotBlank(message = "Here you can have title for task")
+        @Size(min = 3, max = 25)
+        String title,
+
+        String description,
+        @NotNull
+        @Future(message = "Deadline can`t be in past")
+        LocalDateTime deadline
+) {
+}
