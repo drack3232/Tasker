253e5d70175d23d1ef78f7188027d2347abefd4b
drack <artemsenuv@gmail.com>
2026-07-27 20:42:38 +0300

    Prepare and use Spring Security

--- src/main/java/com/trasker/Tasker/Entity/User.java
+++ src/main/java/com/trasker/Tasker/Entity/User.java
@@ -3,12 +3,17 @@ package com.trasker.Tasker.Entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Table(name = "users")
 @Getter
 @Setter
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -18,5 +23,25 @@ public class User {
 
     @Column(nullable = false)
     private String password;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {return true;}
+    @Override
+    public boolean isAccountNonLocked() {return true;}
+    @Override
+    public boolean isCredentialsNonExpired() {return true;}
+    @Override
+    public boolean isEnabled() {return true;}
+
 
 }
